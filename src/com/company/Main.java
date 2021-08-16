fb9fda93b9eefef7654add81bc2fcad79b973fcd
DanValnicek <danvalnicek@gmail.com>
2021-08-16 16:47:01 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        try {
+            new Server("127.0.0.1", 56850);
+            while (true) {
+                Thread.sleep(10 * 1000);
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}

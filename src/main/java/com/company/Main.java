41d67a69914e7553b4ac4ee8a65c83b4a2cc69bc
DanValnicek <danvalnicek@gmail.com>
2021-10-31 14:43:20 +0100

    sql connection testing update

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -6,7 +6,8 @@ import java.sql.SQLException;
 public class Main {
 
 
-	public static void main(String[] args) throws IOException, SQLException {
+	public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException {
+		Class.forName("com.mysql.jdbc.Driver");
 		System.out.println("Enter db password for workServ:");
 		DBHandler.connect(new String(System.console().readPassword()));
 		new Server(56850).run();

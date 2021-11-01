49b4222eb295d31ae772b077369b54a2b01b3990
DanValnicek <danvalnicek@gmail.com>
2021-11-01 13:17:42 +0100

    attempt to register

--- src/main/java/com/company/DBHandler.java
+++ src/main/java/com/company/DBHandler.java
@@ -27,11 +27,11 @@ public class DBHandler {
 	}
 
 	public ResultSet executeQuery(String queryKey, String rawData) throws SQLException {
-		preparedStatement = connection.prepareStatement(queryKey);
+		preparedStatement = connection.prepareStatement(queries.get(queryKey));
 		String[] values = rawData.split(",");
-		for (int i = 0; i < values.length; i++) {
-			preparedStatement.setObject(i+1, values[i]);
-		}
+//		for (int i = 0; i < values.length; i++) {
+			preparedStatement.setObject(1, values);
+//		}
 		resultSet = preparedStatement.executeQuery();
 		return resultSet;
 	}

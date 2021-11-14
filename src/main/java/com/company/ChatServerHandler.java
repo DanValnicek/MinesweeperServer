71c25499590dda00a9f738a10e2a9cb806386b6e
DanValnicek <danvalnicek@gmail.com>
2021-11-14 22:31:05 +0100

    bug fix when rejoinning with the same user

--- src/main/java/com/company/ChatServerHandler.java
+++ src/main/java/com/company/ChatServerHandler.java
@@ -18,7 +18,7 @@ public class ChatServerHandler extends SimpleChannelInboundHandler<String> {
 		Channel incoming = ctx.channel();
 		System.out.println(incoming.toString());
 		System.out.println(channels);
-		if (!channels.contains(incoming)) channels.add(incoming);
+		if (!incoming.isRegistered()) channels.add(incoming);
 		for (Channel channel : channels) {
 			channel.writeAndFlush("[SERVER] - " + incoming.remoteAddress() + " has joined!\n");
 			System.out.println("[SERVER] - " + incoming.remoteAddress() + " has joined!\n");
